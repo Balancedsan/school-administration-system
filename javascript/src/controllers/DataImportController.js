cb7ece9c56e69ad403a4337343a00281c8a7fdec
john <john.wee@flex-solver.com.sg>
2020-08-27 20:32:44 +0800

    update migrations

--- javascript/src/controllers/DataImportController.js
+++ javascript/src/controllers/DataImportController.js
@@ -3,6 +3,7 @@ import { NO_CONTENT } from 'http-status-codes';
 import Logger from '../config/logger';
 import upload from '../config/multer';
 import { convertCsvToJson } from '../utils';
+import model from "../models/index";
 
 const DataImportController = Express.Router();
 const LOG = new Logger('DataImportController.js');
@@ -14,15 +15,17 @@ const dataImportHandler = async (req, res, next) => {
 
   try {
     const data = await convertCsvToJson(file.path);
-    LOG.info(JSON.stringify(data, null, 2));
 
+
+    console.log(test);
+    LOG.info(JSON.stringify(data, null, 2));
   } catch (err) {
-    LOG.error(err)
+    LOG.error(err);
     return next(err);
   }
 
   return res.sendStatus(NO_CONTENT);
-}
+};
 
 DataImportController.post('/upload', upload.single('data'), dataImportHandler);
 
