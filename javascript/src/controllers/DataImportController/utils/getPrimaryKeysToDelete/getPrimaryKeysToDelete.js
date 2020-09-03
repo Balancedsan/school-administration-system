04a99cbcacd3ef31192c2dcc74a005e9963e6488
john <john.wee@flex-solver.com.sg>
2020-09-03 21:07:17 +0800

    remove uncessary functions in getprimarykeytodelete

--- javascript/src/controllers/DataImportController/utils/getPrimaryKeysToDelete/getPrimaryKeysToDelete.js
+++ javascript/src/controllers/DataImportController/utils/getPrimaryKeysToDelete/getPrimaryKeysToDelete.js
@@ -1,32 +1,11 @@
-import getCompsiteKeys from '../getCompositeKeys/getCompositeKeys';
 import getPrimaryKey from '../getPrimaryKey/getPrimaryKey';
-import breakCompositeKeys from '../breakCompositeKeys/breakCompositeKeys';
 
 const getPrimaryKeyToDelete = (csvData) => {
   const teacherKey = getPrimaryKey(csvData, 'teacherEmail');
   const studentKey = getPrimaryKey(csvData, 'studentEmail');
   const classKey = getPrimaryKey(csvData, 'classCode');
   const subjectKey = getPrimaryKey(csvData, 'subjectCode');
-  const teacherStudentKeys = breakCompositeKeys(getCompsiteKeys(
-    csvData,
-    'teacherEmail',
-    'studentEmail'
-  ));
-  const teacherClassKeys = breakCompositeKeys(getCompsiteKeys(
-    csvData,
-    'teacherEmail',
-    'classCode'
-  ));
-  const teacherSubjectKeys = breakCompositeKeys(getCompsiteKeys(
-    csvData,
-    'teacherEmail',
-    'subjectCode'
-  ));
-  const classStudentKeys = breakCompositeKeys(getCompsiteKeys(
-    csvData,
-    'classCode',
-    'studentEmail'
-  ));
+
 
 
   return [
@@ -34,10 +13,6 @@ const getPrimaryKeyToDelete = (csvData) => {
     studentKey,
     classKey,
     subjectKey,
-    teacherStudentKeys,
-    teacherClassKeys,
-    teacherSubjectKeys,
-    classStudentKeys,
   ];
 };
 
