cfe0d57e735e4499bf6e2de33bebad15930e5c04
john <john.wee@flex-solver.com.sg>
2020-09-03 22:18:07 +0800

    update router

--- javascript/src/router.js
+++ javascript/src/router.js
@@ -1,10 +1,12 @@
 import Express from 'express';
 import DataImportController from './controllers/DataImportController/DataImportController';
 import HealthcheckController from './controllers/HealthcheckController';
+import StudentListingController from './controllers/StudentListingController/StudentListingController';
 
 const router = Express.Router();
 
 router.use('/', DataImportController);
 router.use('/', HealthcheckController);
+router.use('/', StudentListingController);
 
 export default router;
